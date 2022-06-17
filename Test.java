8b941547d0a46946c59693c18fc724472504ff5b
gui <271743471@qq.com>
2022-06-17 15:36:08 +0800

    Update Test.java

--- Test.java
+++ Test.java
@@ -2,6 +2,8 @@ import java.util.*;
 
 public class Test {
   
+  private static final int aa = 1;
+  
   public void test() {
     int a = 1;
     int b = a + 1;
@@ -15,6 +17,10 @@ public class Test {
   public static void main(String[] args) {
     Test test = new Test();
     test.test();
+    aa = 2;
+  }
+  
+  static class A {
   }
   
 }
