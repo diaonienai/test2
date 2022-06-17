5124b7526b5eb55122d4180ae05d17f2069986b3
gui <271743471@qq.com>
2022-06-17 14:46:13 +0800

    Create Test.java

---
+++ Test.java
@@ -0,0 +1,16 @@
+package com.gui.test;
+
+import java.util.*;
+
+public class Test {
+  
+  public void test() {
+    int a = 1;
+    int b = a + 1;
+  }
+
+  public static void main(String[] args) {
+
+  }
+  
+}
